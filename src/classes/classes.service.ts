08ecb48e35548fb85f5c0a42122c0a864e4e629b
tony-div <tg764562@gmail.com>
2024-09-23 08:19:43 +0300

    feat: implement class service & cleanup

--- src/classes/classes.service.ts
+++ src/classes/classes.service.ts
@@ -1,15 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateClassDto } from './dto/create-class.dto';
-import { UpdateClassDto } from './dto/update-class.dto';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class ClassesService {
   constructor(private readonly databaseService: DatabaseService) {}
-
-  create(createClassDto: CreateClassDto) {
-    return 'This action adds a new class';
-  }
 
   async findAll() {
     const classes = await this.databaseService.executeQuery(
@@ -20,7 +14,7 @@ export class ClassesService {
 
   async findOne(id: number) {
     const result = await this.databaseService.executeQuery(
-      'select class_id, class_name from classes where id = ?;',
+      'select class_id, class_name from classes where class_id = ?;',
       [id],
     );
     return result;
@@ -28,17 +22,28 @@ export class ClassesService {
 
   async getStudents(id: number) {
     const students = await this.databaseService.executeQuery(
-      'select student_id, student_name, address, photo_link, google_maps_home_link, phone_number from students inner join student_class using(student_id) inner join phone_numbers using(student_id) where class_id = ?;',
+      `
+      select 
+        student_id, 
+        student_name, 
+        address, 
+        group_concat(phone_number separator ', ') as phone_numbers,
+        district,
+        notes
+      from students 
+      inner join 
+        student_class using(student_id) 
+      left join 
+        phone_numbers using(student_id) 
+      where class_id = ? 
+      group by student_id;`,
       [id],
     );
-    return students;
-  }
 
-  update(id: number, updateClassDto: UpdateClassDto) {
-    return `This action updates a #${id} class`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} class`;
+    const metadata = await this.databaseService.executeQuery(
+      'select count(student_id) as count from student_class where class_id = ?',
+      [id],
+    );
+    return { students, metadata };
   }
 }
