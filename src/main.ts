717fcda63a680d12c2806a15cdd040c0c7e5d5ee
Tony George <tg764562@gmail.com>
2024-09-24 14:50:45 +0300

    chore: cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-
+import { readFileSync } from 'fs';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {logger: ['error', 'log', 'warn']});
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'log', 'warn'],
+  });
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT');
   app.enableCors();
