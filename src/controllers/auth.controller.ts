0915cdd54bbad682302d0d9139de4193cc48e28b
Tony George <tg764562@gmail.com>
2025-07-12 21:45:38 +0300

    fix: response schemas

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
 import { SignInDto } from "../schemas/auth.schemas";
 import authService from "../services/auth.service";
+import { StatusCodes } from "http-status-codes/build/cjs/status-codes";
 
 export async function signIn(req: Request, res: Response) {
   const credentials: SignInDto = req.body;
   const result = await authService.signIn(credentials.username, credentials.password)
-  res.json(result);
+  res.status(StatusCodes.OK).json({success: true, data: result});
 }
 
 export async function verifyToken(req: Request, res: Response) {
   const result = authService.verify(req.body.token);
-  res.json(result);
+  res.status(StatusCodes.OK).json({success: true, data: result});
 }
--- src/controllers/classes.controller.ts
+++ src/controllers/classes.controller.ts
@@ -1,10 +1,11 @@
+import { StatusCodes } from "http-status-codes";
 import classesService from "../services/classes.service";
 import { Request, Response } from "express";
 
 export async function getClasses(_req: Request, res: Response) {
   const userId: number = res.locals["user"]["sub"];
   const classes = await classesService.getUserJoinedClasses(userId);
-  res.json(classes);
+  res.status(StatusCodes.OK).json({success: true, data: classes});
 };
 
 export async function getStudents(req: Request, res: Response) {
@@ -12,7 +13,7 @@ export async function getStudents(req: Request, res: Response) {
   if(classId) {
     classId = parseInt(classId);
     const students = await classesService.getStudents(classId);
-    res.json(students);
+    res.status(StatusCodes.OK).json({success: true, data: students});
   }
   res.status(400).json({ error: "Invalid class ID" }); // handle this in the global error handler
 }
@@ -22,9 +23,7 @@ export async function getTeachers(req: Request, res: Response) {
   if(classId) {
     classId = parseInt(classId);
     const teachers = await classesService.getTeachers(classId);
-    res.json(teachers);
+    res.status(StatusCodes.OK).json({success: true, data: teachers});
   }
   res.status(400).json({ error: "Invalid class ID" }); // handle this in the global error handler
 }
-
-export default { getStudents, getTeachers };
