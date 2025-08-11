139714db36545c01d9bf0d8272b10a69cff4aa2e
Tony George <tg764562@gmail.com>
2025-08-11 20:15:50 +0300

    feat(persons.service): input sanitization

--- src/services/persons.service.ts
+++ src/services/persons.service.ts
@@ -16,22 +16,22 @@ async function createPerson(type: 'student' | 'teacher', data: CreatePersonDto) 
     const [personResult] = await connection.query<ResultSetHeader>(
       'insert into persons(person_name, address, district_id, notes) values (?, ?, ?, ?);',
       [
-        data.name,
-        data.address,
+        data.name.trim(),
+        data.address.trim(),
         data.district_id,
-        data.notes,
+        data.notes?.trim(),
       ],
     );
 
     await connection.query(
       'insert into phone_numbers(person_id, phone_number) values (?, ?);',
-      [personResult.insertId, data.phone_number],
+      [personResult.insertId, data.phone_number.trim()],
     );
 
     if (data.second_phone_number != undefined) {
       await connection.query(
         'insert into phone_numbers(person_id, phone_number) values (?, ?);',
-        [personResult.insertId, data.second_phone_number],
+        [personResult.insertId, data.second_phone_number.trim()],
       );
     }
 
@@ -59,10 +59,10 @@ async function updatePerson(personId: number, person: UpdatePersonDto) {
         set person_name=?, address=?, district_id=?, notes=? 
         where person_id=? and person_id in (select person_id from person_class where type='student' and person_id=?);`,
         [
-          person.name,
-          person.address,
+          person.name.trim(),
+          person.address.trim(),
           person.district_id,
-          person.notes,
+          person.notes?.trim(),
           personId,
           personId,
         ],
@@ -77,7 +77,7 @@ async function updatePerson(personId: number, person: UpdatePersonDto) {
       if (phoneNumbersIds[0]) {
         await connection.query(
           'update phone_numbers set phone_number=? where person_id=? and phone_number_id=?',
-          [person.phone_number, personId, phoneNumbersIds[0].phone_number_id],
+          [person.phone_number.trim(), personId, phoneNumbersIds[0].phone_number_id],
         );
       }
 
@@ -85,13 +85,13 @@ async function updatePerson(personId: number, person: UpdatePersonDto) {
         if (phoneNumbersIds[1] == undefined)
           await connection.query(
             'insert into phone_numbers(person_id, phone_number) values (?, ?);',
-            [personId, person.second_phone_number],
+            [personId, person.second_phone_number.trim()],
           );
         else
           await connection.query(
             'update phone_numbers set phone_number=? where person_id=? and phone_number_id=?',
             [
-              person.second_phone_number,
+              person.second_phone_number.trim(),
               personId,
               phoneNumbersIds[1].phone_number_id,
             ],
