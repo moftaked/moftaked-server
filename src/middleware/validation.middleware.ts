6cb77d321badcb8e7c23cd96975590c3c9d7ae60
Tony George <tg764562@gmail.com>
2025-08-12 23:56:05 +0300

    chore: global error handler now handles validation errors

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,26 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-import { z, ZodError } from 'zod/v4';
-
-import { StatusCodes } from 'http-status-codes';
+import { z } from 'zod/v4';
 
 export function validateData(schema: z.ZodObject) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      schema.parse(req.body);
-      next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        const errorMessages = error.issues.map(issue => ({
-          message: `${issue.path.join('.')} is ${issue.message}`,
-        }));
-        res
-          .status(StatusCodes.BAD_REQUEST)
-          .json({ error: 'Invalid data', details: errorMessages });
-      } else {
-        res
-          .status(StatusCodes.INTERNAL_SERVER_ERROR)
-          .json({ error: 'Internal Server Error' });
-      }
+  return (req: Request, _res: Response, next: NextFunction) => {
+    let result = schema.safeParse(req.body);
+    if (!result.success) {
+      next(result.error);
+      return;
     }
+    next();
   };
 }
