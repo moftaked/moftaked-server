c77ceee8f80a72505b383b2f359297106a5956ed
Tony George <tg764562@gmail.com>
2025-08-18 00:56:32 +0300

    fix: better error handling

--- src/controllers/attendance.controller.ts
+++ src/controllers/attendance.controller.ts
@@ -1,14 +1,19 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import attendanceService from '../services/attendance.service';
 import { PatchAttendanceDto } from '../schemas/attendance.schemas';
+import createHttpError from 'http-errors';
 
 export function patchAttendance(type: 'student' | 'teacher') {
-  return async (req: Request, res: Response) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     const body: PatchAttendanceDto = req.body;
+    const eventOccurrenceId = parseInt(req.params['eventOccurrenceId']!);
+    if (isNaN(eventOccurrenceId)) {
+      return next(createHttpError(400, 'Event occurrence ID is required'));
+    }
     await attendanceService.patchAttendance(
       body.attended,
       body.absent,
-      parseInt(req.params['eventOccurrenceId'] || ''),
+      eventOccurrenceId,
       type,
     );
     res.json({ message: 'Attendance updated successfully' });
