7587dcdd1be789dc28eb35cdd81b32132b0058be
Tony George <tg764562@gmail.com>
2025-08-18 04:25:06 +0300

    feat(auth): error classes for service layer

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,15 +1,19 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { SignInDto } from '../schemas/auth.schemas';
 import authService from '../services/auth.service';
 import { StatusCodes } from 'http-status-codes/build/cjs/status-codes';
 
-export async function signIn(req: Request, res: Response) {
+export async function signIn(req: Request, res: Response, next: NextFunction) {
   const credentials: SignInDto = req.body;
-  const result = await authService.signIn(
-    credentials.username,
-    credentials.password,
-  );
-  res.status(StatusCodes.OK).json({ success: true, data: result });
+  try {
+    const result = await authService.signIn(
+      credentials.username,
+      credentials.password,
+    );
+    res.status(StatusCodes.OK).json({ success: true, data: result });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export async function verifyToken(req: Request, res: Response) {
